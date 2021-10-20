90c968fd46935c8548d165b7a6904f79f0a6834f
stdweird <stijn.deweirdt@ugent.be>
2021-10-20 13:12:30 +0200

    aii-core: add ansible playbook, role and task module

---
+++ aii-core/src/main/perl/Role.pm
@@ -0,0 +1,46 @@
+#${PMpre} AII::Role${PMpost}
+
+use LC::Exception qw (SUCCESS);
+use parent qw(CAF::Object);
+
+use AII::Task;
+
+# name: name of role
+sub _initialize
+{
+    my ($self, $name, %opts) = @_;
+
+    %opts = () if !%opts;
+
+    $self->{log} = $opts{log} if $opts{log};
+
+    $self->{name} = $name;
+    $self->{data} = {
+    };
+    $self->{tasks} = [];
+
+    return SUCCESS;
+}
+
+sub add_task
+{
+    my ($self, $name) = @_;
+    my $task = AII::Task->new($name, log => $self);
+    push @{$self->{tasks}}, $task;
+    return $task;
+}
+
+# Generate hashref to render into yaml
+sub make_data {
+    my $self = shift;
+
+    # make copy of basic data
+    my $data = {%{$self->{data}}};
+
+    # add tasks
+    $data->{tasks} = [map {$_->make_data()} @{$self->{tasks}}];
+
+    return $data;
+}
+
+1;
---
+++ aii-core/src/main/perl/Task.pm
@@ -0,0 +1,35 @@
+#${PMpre} AII::Task${PMpost}
+
+use LC::Exception qw (SUCCESS);
+use parent qw(CAF::Object);
+
+# name: name of task
+sub _initialize
+{
+    my ($self, $name, %opts) = @_;
+
+    %opts = () if !%opts;
+
+    $self->{log} = $opts{log} if $opts{log};
+
+    $self->{name} = $name;
+    $self->{data} = {
+    };
+
+    return SUCCESS;
+}
+
+# Generate hashref to render into yaml
+sub make_data {
+    my $self = shift;
+
+    # make copy of basic data
+    my $data = {%{$self->{data}}};
+
+    # add tasks
+    $data->{name} = $self->{name};
+
+    return $data;
+}
+
+1;
